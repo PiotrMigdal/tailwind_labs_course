5884d42a4a11d9d0e048dddabc8e51459198b98b
PiotrMigdal <Piotr.Migdal@PARSEQ.COM>
2021-09-18 14:18:55 +0100

    Custom classes and components. End of course

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: "#d050d2",
+          DEFAULT: "#d10ad4", //This must be DEFAULT so you can use just bg-brand
+          dark: "#b400b7",
+        },
+      },
+      fontFamily: {
+        headline: "Tangerine, serif" //font-headline
+      },
+    },
   },
   variants: {
     extend: {
